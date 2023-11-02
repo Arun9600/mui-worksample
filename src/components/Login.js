d53e0b8b31f177733724eefa025b68b05c44f22e
Arun <69082994+Arun9600@users.noreply.github.com>
2023-11-02 12:29:10 +0530

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,70 @@
 import { Button, Container, FormControl, TextField, Box } from "@mui/material";
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
   return (
     <>
       <Box sx={{ backgroundColor: "#fff" }} padding={6}>
-        <FormControl fullWidth required>
-          <TextField
-            type="email"
-            label="Email"
-            name="email"
-            sx={{ marginBottom: "25px" }}
-            inputProps={{
-              style: {
-                height: "20px",
-              },
-            }}
-          ></TextField>
-        </FormControl>
-        <FormControl fullWidth required>
-          <TextField
-            type="password"
-            label="Password"
-            name="password"
-            sx={{ marginBottom: "25px" }}
-            inputProps={{
-              style: {
-                height: "20px",
-              },
-            }}
-          ></TextField>
-        </FormControl>
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Button variant="contained" size="large" type="submit">
-            Submit
-          </Button>
-        </Box>
+        <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+          <FormControl fullWidth required>
+            <TextField
+              type="email"
+              label="Email"
+              name="email"
+              sx={{ marginBottom: "25px" }}
+              inputProps={{
+                style: {
+                  height: "20px",
+                },
+              }}
+              {...register("email", {
+                required: "Email is Required",
+                pattern: {
+                  value:
+                    /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+                  message: "Enter the valid email address",
+                },
+              })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            ></TextField>
+          </FormControl>
+          <FormControl fullWidth required>
+            <TextField
+              type="password"
+              label="Password"
+              name="password"
+              sx={{ marginBottom: "25px" }}
+              inputProps={{
+                style: {
+                  height: "20px",
+                },
+              }}
+              {...register("password", {
+                required: "Password is Required",
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters long",
+                },
+              })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            ></TextField>
+          </FormControl>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button variant="contained" size="large" type="submit">
+              Login
+            </Button>
+          </Box>
+        </form>
       </Box>
     </>
   );
